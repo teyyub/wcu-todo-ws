3cea016ec5f7edc651d53b0939fff556194aa8d6
teyyub <teyyub392@gmail.com>
2024-05-23 09:23:12 +0400

    command line runner

--- src/main/java/com/example/todo/WcuTodoWsApplication.java
+++ src/main/java/com/example/todo/WcuTodoWsApplication.java
@@ -17,16 +17,6 @@ public class WcuTodoWsApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Todo t1 = new Todo("desc 1" , "note 1");
-		Todo t2 = new Todo("desc 2" , "note 2");
-
-		String url = "localhost:8082/api/todos";
-
-		HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create(url))
-				.header("Content-Type", "application/json")
-				.POST(HttpRequest.BodyPublishers.ofString(String.valueOf(t1)))
-				.build();
-
+		System.out.println("welcome wcu todo web service");
 	}
 }

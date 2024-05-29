7fd04af35a278127390214192b4e99817240ff1f
teyyub <teyyub392@gmail.com>
2024-05-30 03:06:35 +0400

    remove postgresql_data

--- postgres/init.sql
+++ postgres/init.sql
@@ -7,6 +7,7 @@ CREATE TABLE todos (
 
 INSERT INTO todos (description, note, created_at)
 VALUES
+
     ('Complete homework assignment', 'Due by Friday', '2024-05-30 10:00:00'),
     ('Buy groceries', 'Milk, bread, eggs', '2024-05-30 12:30:00'),
     ('Call mom', 'Wish her a happy birthday', '2024-05-30 15:45:00');
